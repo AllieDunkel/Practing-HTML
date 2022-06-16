82b9adfa2eb2e5b5d92f87c0159eb2d23f1edc00
Allie Dunkel <alliedunkel@outlook.com>
2022-06-16 13:00:54 -0500

    Added some javascript and worked more on css

--- js/app.js
+++ js/app.js
@@ -3,30 +3,45 @@ console.log('app is connected.');
 
 // prompt is assinged to the userName//   
 
-let favFood = prompt('What is your favorite food?');
-console.log('is this pizza', favFood);
-let message; 
+// let favFood = prompt('What is your favorite food?');
+// console.log('is this pizza', favFood);
+// let message; 
 
 
-alert('You entered ' + favFood + '!');
+// alert('You entered ' + favFood + '!');
 
-if(favFood == 'pizza') {
-    message = 'We got that!';
-} else if( favfood == 'burgers') {
-    message = 'Come on over!!';
-} else if (favfood == 'nachos') {
-    message = 'We got those!';
-} else if (favfood == 'sandwiches') {
-    message = 'We got those!';
-} else if (favfood == 'salad') {
-        message = 'We got those!';
-} else {
-    message = 'We are sorry, but we have acholol!';
-}
+// if(favFood == 'pizza') {
+//     message = 'We got that!';
+// } else if( favfood == 'burgers') {
+//     message = 'Come on over!!';
+// } else if (favfood == 'nachos') {
+//     message = 'We got those!';
+// } else if (favfood == 'sandwiches') {
+//     message = 'We got those!';
+// } else if (favfood == 'salad') {
+//         message = 'We got those!';
+// } else {
+//     message = 'We are sorry, but we have acholol!';
+// }
 
-console.log('our return message', message);
-console.log('our return message', favFood);
+// console.log('our return message', message);
+// console.log('our return message', favFood);
 
-document.write('your favorite food is ' + favFood);
+// document.write('your favorite food is ' + favFood);
 
 
+function favFood(){
+    let userfavFood = prompt('what is your favorite food?');
+    let message;
+  
+    if(favFood === 'pizza' || favFood === 'burgers' ||  favFood ==='salads' || favFood === 'nachos'){
+      let userfavFood = prompt('What is your favorite food?');
+    } else {
+      message = 'we got those' ;
+    }
+   return message;
+  
+  }
+  
+  favFood();
+  
---
+++ js/functiondemo.js
@@ -0,0 +1,3 @@
+'use strict'
+console.log('function demo js is connection.');
+
